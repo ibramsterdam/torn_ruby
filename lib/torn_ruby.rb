a874ef72c9a883ba8c479a3008ef3801eb6b864d
Bram <ibramsterdam@hotmail.com>
2025-04-18 19:13:23 +0200

    feat: gem setup

---
+++ lib/torn_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "torn_ruby/version"
+
+module TornRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/torn_ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TornRuby do
+  it "has a version number" do
+    expect(TornRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
